f4424c1705b21739eea07bb81d21e7f0dd1e18f2
tbhaxor <tbhaxor@gmail.com>
2018-03-16 01:08:20 +0530

    added header file

---
+++ firefly.hpp
@@ -0,0 +1,15 @@
+#ifndef FIREFLY_H
+#define FIREFLY_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Vectors {
+    vector<float> vec;
+public:
+    Vectors(vector<float> vector);
+    void print();
+};
+
+#endif
