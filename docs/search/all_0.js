32ef3662537c31e4da82b60922396951db94df81
Gurkirat Singh <tbhaxor@gmail.com>
2023-07-25 12:33:41 +0530

    improve (docs): rebuild doxygen creating anti_parallel.cpp documentation

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -4,6 +4,7 @@ var searchData=
   ['add_2ecpp_1',['add.cpp',['../add_8cpp.html',1,'']]],
   ['angle_5fwith_2ecpp_2',['angle_with.cpp',['../angle__with_8cpp.html',1,'']]],
   ['anglewith_3',['AngleWith',['../classFirefly_1_1Vector.html#a1f27be5b4d8f2d41ba4fcc3fcde91a7d',1,'Firefly::Vector']]],
-  ['area_2ecpp_4',['area.cpp',['../area_8cpp.html',1,'']]],
-  ['at_5',['At',['../classFirefly_1_1Vector.html#aafcf2e6a2a012a41a1532560f79d0325',1,'Firefly::Vector::At(std::size_t idx) const'],['../classFirefly_1_1Vector.html#ad256e126e16073c0383cd39c1d48bd9d',1,'Firefly::Vector::At(std::size_t idx)']]]
+  ['anti_5fparallel_2ecpp_4',['anti_parallel.cpp',['../anti__parallel_8cpp.html',1,'']]],
+  ['area_2ecpp_5',['area.cpp',['../area_8cpp.html',1,'']]],
+  ['at_6',['At',['../classFirefly_1_1Vector.html#aafcf2e6a2a012a41a1532560f79d0325',1,'Firefly::Vector::At(std::size_t idx) const'],['../classFirefly_1_1Vector.html#ad256e126e16073c0383cd39c1d48bd9d',1,'Firefly::Vector::At(std::size_t idx)']]]
 ];
