9a02db85477ed8e746227158d9b43aae85662ba0
Gurkirat Singh <tbhaxor@gmail.com>
2023-08-20 22:51:51 +0530

    fix `-Wsign-compare` in is_sparse method

--- src/vector/is_sparse.cpp
+++ src/vector/is_sparse.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <iostream>
 #include <stdexcept>
 
 #include "firefly/vector.hpp"
@@ -9,8 +10,8 @@ bool Vector::IsSparse() const {
     throw std::length_error("Can't determine sparseness of empty vector");
   }
 
-  auto const zero_count = std::count_if(m_vec.cbegin(), m_vec.cend(),
-                                        [](Real const &v) { return v == 0; });
+  size_t const zero_count = std::count_if(m_vec.cbegin(), m_vec.cend(),
+                                          [](Real const &v) { return v == 0; });
 
   return (m_vec.size() - zero_count) < zero_count;
 }
